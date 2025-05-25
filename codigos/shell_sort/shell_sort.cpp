ca977d7de694c006f119c1c61f153e911033fd5a
riAnriAn0 <156357069+riAnriAn0@users.noreply.github.com>
2025-05-25 15:27:23 -0300

    atualizacao

---
+++ codigos/shell_sort/shell_sort.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void iniciar_vetor(int *vetor, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        vetor[i] = abs(rand() % 101);
+    }
+}
+
+void print_vetor(int *vetor, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("[%d] -> ", vetor[i]);
+    }
+}
+
+void trocar(int *vetor, int i, int j)
+{
+    int aux = vetor[i];
+    vetor[i] = vetor[j];
+    vetor[j] = aux;
+}
+
+void shellSort(int *vetor, int n)
+{
+    int gap = (n/ 2);
+
+    while (gap >= 1)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int i = j; i + gap < n; i += gap)
+            {
+                if (vetor[i] > vetor[i + gap])
+                {
+                    trocar(vetor, i, i + gap);
+                }
+            }
+        }
+        gap /= 2;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+
+    srand(time(NULL));
+
+    int tam = 213;
+    int lista[tam];
+
+    iniciar_vetor(lista, tam);
+
+    printf("Lista Desordenada\n");
+    print_vetor(lista, tam);
+
+    shellSort(lista, tam);
+
+    printf("\n\nLista Ordenada\n");
+    print_vetor(lista, tam);
+
+    return 0;
+}
